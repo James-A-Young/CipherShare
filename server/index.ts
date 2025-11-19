51505e07d14a47e5464b674d350965662563d870
James Young <github@jamesy.co.uk>
2025-11-19 11:44:19 +0000

    Hide Express server identifier in responses

--- server/index.ts
+++ server/index.ts
@@ -28,6 +28,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+// Hide Express server identifier in responses
+app.disable('x-powered-by');
 const PORT = process.env.PORT || 3001;
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
